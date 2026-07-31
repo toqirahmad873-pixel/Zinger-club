6aa84e8113f0c5dc3d681c67a24b2b6efc3a36fb
toqirahmad873-pixel <toqirahmad873@gmail.com>
2026-07-31 13:10:27 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,40 +1,21 @@
 // sw.js
-const CACHE_NAME = 'zinger-club-v2';
+const CACHE_NAME = 'zinger-v3';
 
-// Sirf zaroori files cache hongi taake install hone me bilkul time na lage
-const STATIC_ASSETS = [
-  '/',
-  '/manifest-v2.json',
-  'https://i.postimg.cc/qvnF1k3t/Zinger.png'
-];
-
-// Fast Install Event
 self.addEventListener('install', (event) => {
-  self.skipWaiting(); // Doosre pending scripts ka wait kiye bina instant install karega
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(STATIC_ASSETS);
-    })
-  );
+  self.skipWaiting();
 });
 
-// Activate Event
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then((keys) => {
       return Promise.all(
-        cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
+        keys.map((key) => caches.delete(key))
       );
-    })
+    }).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
-// Network-first strategy for menu assets
+// Direct fetch handling - Zero background delay
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
